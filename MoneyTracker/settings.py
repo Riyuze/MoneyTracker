cd658c5626c644fc675d6893ff5cda8b95461ae0
Kevin Tandavo <riyuze211001@gmail.com>
2022-05-21 04:03:36 +0700

    Added PostgreSQL

--- MoneyTracker/settings.py
+++ MoneyTracker/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
+# Library to load env files
+from dotenv import load_dotenv
+
+# Loads the env files
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +83,11 @@ WSGI_APPLICATION = 'MoneyTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
