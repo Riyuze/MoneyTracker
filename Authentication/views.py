cc8d8ab9734a09a61838930b7b3f3fae3fbf0492
Kevin Tandavo <riyuze211001@gmail.com>
2022-05-25 20:38:51 +0700

    Added user activation

--- Authentication/views.py
+++ Authentication/views.py
@@ -7,7 +7,7 @@ from validate_email import validate_email
 from django.contrib import messages
 from django.core.mail import EmailMessage
 
-from django.utils.encoding import force_bytes, DjangoUnicodeDecodeError
+from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 
@@ -69,7 +69,7 @@ class RegistrationView(View):
                 domain = get_current_site(request).domain
                 link = reverse('activate', kwargs={'uidb64' : uidb64, 'token' : token})
 
-                activate_url = "https://" + domain + link
+                activate_url = "http://" + domain + link
 
                 email_subject = "Activate your account"
                 email_body = "Hi " + user.username + ", please use the link below to activate your account\n" + activate_url
@@ -81,11 +81,33 @@ class RegistrationView(View):
                 )
                 email.send(fail_silently=False)
 
-                messages.success(request, 'Account successfully created!')
+                messages.success(request, 'Account successfully created, please check your email for activation instructions!')
                 return render(request, 'authentication/register.html')
 
         return render(request, 'authentication/register.html')
 
 class VerificationView(View):
     def get(self, request, uidb64, token):
+        try:
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk = id)
+
+            if not token_generator(user, token):
+                return redirect('login' + '?message=' + 'Account already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account activated succesfully!')
+            return redirect('login')
+
+        except Exception as ex:
+            pass
+
         return redirect('login')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'authentication/login.html')
