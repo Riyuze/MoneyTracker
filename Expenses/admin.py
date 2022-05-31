015ea4bd587ff2b3eb48669acf1a8baa3bf981c7
Kevin Tandavo <riyuze211001@gmail.com>
2022-06-01 01:01:10 +0700

    Added basic add expenses form

--- Expenses/admin.py
+++ Expenses/admin.py
@@ -1,4 +1,8 @@
+from sre_constants import CATEGORY_DIGIT
 from django.contrib import admin
+from .models import  Expense, Category
 
 # Register your models here.
 
+admin.site.register(Expense)
+admin.site.register(Category)
--- Expenses/models.py
+++ Expenses/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 from django.utils.timezone import now 
 from django.contrib.auth.models import User
@@ -6,18 +5,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Expense(models.Model):
-    amount = models.FloatField()
+    amount = models.DecimalField(decimal_places=2, max_digits=20)
     date = models.DateField(default=now)
     description = models.TextField()
-    user=models.ForeignKey(to=User, on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     category = models.CharField(max_length=255)
 
     def __str__(self):
         return self.category
 
     class Meta:
-        ordering: ['-date']
+        ordering = ['-date']
 
-    class Category(models.Model):
-        name = models.CharField(max_length=255)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
