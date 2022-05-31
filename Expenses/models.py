d85bc58b7b8adf958ce7826fd1ee3cfec263d3c5
Kevin Tandavo <riyuze211001@gmail.com>
2022-06-01 06:43:32 +0700

    Added working expense page

--- Expenses/models.py
+++ Expenses/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth.models import User
 
 class Expense(models.Model):
     amount = models.DecimalField(decimal_places=2, max_digits=20)
-    date = models.DateField(default=now)
     description = models.TextField()
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     category = models.CharField(max_length=255)
+    date = models.DateField(default=now)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.category
--- Expenses/views.py
+++ Expenses/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
 
 # Create your views here.
 
 @login_required(login_url='authentication/login')
 
 def index(request):
-    return render(request, 'expenses/index.html')
+    expenses = Expense.objects.filter(user=request.user)
+
+    context = {
+        'expenses': expenses,
+    }
+
+    return render(request, 'expenses/index.html', context)
 
 def add_expenses(request):
-    return render(request, 'expenses/add_expenses.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+
+    if request.method == 'GET':  
+        return render(request, 'expenses/add_expenses.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expenses.html', context)
+
+        if not description:
+            messages.error(request, 'Description is required')
+            return render(request, 'expenses/add_expenses.html', context)
+    
+        Expense.objects.create(user=request.user, amount=amount, description=description, date=date, category=category)
+
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('expenses')
