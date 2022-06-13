5cbb84bcad909237ec8e87e2c3b183c547b30e30
Kevin Tandavo <riyuze211001@gmail.com>
2022-06-13 20:54:32 +0700

    Added Incomes Charts

--- Incomes/urls.py
+++ Incomes/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('edit-incomes/<int:id>', views.edit_incomes, name="edit-incomes"),
     path('delete-incomes/<int:id>', views.delete_incomes, name="delete-incomes"),
     path('search-incomes', csrf_exempt(views.search_incomes), name="search-incomes"),
+    path("income-summary", views.income_summary, name="income-summary"),
+    path('summary_rest', views.income_summary_rest, name='income_summary_rest'),
+    path('three_months_summary', views.last_3months_income_stats, name='three_months_summary_'),
+    path('last_3months_income_source_stats', views.last_3months_income_source_stats, name='last_3months_income_source_stats_'),
 ]
