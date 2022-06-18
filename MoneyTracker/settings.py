04074b24a203203326df4c756775278ecfc246ef
Kevin Tandavo <riyuze211001@gmail.com>
2022-06-19 04:18:21 +0700

    Hid Secret Key

--- MoneyTracker/settings.py
+++ MoneyTracker/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-14j=3iy$tvcz4zp!ysoh@wc(agd6ykd&*q(ec@g$0p9a(n6j7s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
